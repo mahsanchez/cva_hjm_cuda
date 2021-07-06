189450712d93e41be155cb8560418f03f5fcf384
mahsanchez <mahsanchez@users.noreply.github.com>
2021-07-06 10:51:58 +0200

    Run simulations with Double precision

--- product.h
+++ product.h
@@ -15,4 +15,18 @@ struct InterestRateSwap {
     float expiry;
 };
 
+struct InterestRateSwap_D {
+    InterestRateSwap_D(double* pricing_points_, double* floating_schedule_, double* fixed_schedule_, double notional_, double K_, double expiry_, double dtau_) :
+        pricing_points(pricing_points_), floating_schedule(floating_schedule_), fixed_schedule(fixed_schedule_), notional(notional_), K(K_), expiry(expiry_), dtau(dtau_)
+    {}
+
+    double* pricing_points;
+    double* floating_schedule;
+    double* fixed_schedule;
+    double notional;
+    double K;
+    double dtau;
+    double expiry;
+};
+
 #endif
